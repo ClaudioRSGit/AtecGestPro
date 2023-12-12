a6563d059ceef9818de55ddf56637d24cf9f38fa
Ricardo Ferreira <ricardo.ferreira.t0123185@edu.atec.pt>
2023-12-12 11:08:59 +0000

    changed create_partners_table name. added seed for partners

--- Laravel/database/seeds/PartnerSeeder.php
+++ Laravel/database/seeds/PartnerSeeder.php
@@ -11,6 +11,33 @@ class PartnerSeeder extends Seeder
      */
     public function run()
     {
-        //
-    }
+        DB::table('partners')->insert([
+            'name' => 'Amkor Technology Portugal (ATEP)',
+            'description' => '',
+            'address' => 'Avenida 1º De Maio, 801 Mindelo, 4485-629 Porto',
+        ]);
+
+        DB::table('partners')->insert([
+            'name' => 'ENGIE - Douro',
+            'description' => '',
+            'address' => 'Barragem do Picote-Sendim, 5225-072 Miranda Do Douro',
+        ]);
+
+        DB::table('partners')->insert([
+            'name' => 'Ordem dos Engenheiros - Região Centro',
+            'description' => '',
+            'address' => 'Rua Antero de Quental, 107, 3000-032 Coimbra',
+        ]);
+
+        DB::table('partners')->insert([
+            'name' => 'Netos Shoes',
+            'description' => '',
+            'address' => 'R. António Luís da Costa 100, 3700-310 São João da Madeira',
+        ]);
+
+        DB::table('partners')->insert([
+            'name' => 'GRANCRUZ',
+            'description' => '',
+            'address' => 'Rua José Mariani 390, 4400-195 VNGaia',
+        ]);    }
 }
