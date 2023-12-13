2b61b8dd569b314e5c4996c95a2dc32bd2705c42
Claudio <claudio.silva.t0123173@edu.atec.pt>
2023-12-13 22:18:09 +0000

    Routes

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,3 +9,17 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('dashboard', 'DashboardController');
+
+Route::resource('users', 'UserController');
+
+Route::resource('tickets', 'TicketController');
+
+Route::resource('material', 'MaterialController');
+
+Route::resource('trainings', 'TrainingsController');
+
+Route::resource('clothing', 'ClothingController');
+
+Route::resource('classes', 'CourseClassController');
