acbcdb49e3d68d4276c0c669b0ed402c6e308a12
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2023-12-13 20:57:32 +0000

    Relationships created in the Clothing_Delivery, Ticket_History and its dependents

--- Laravel/app/Clothing_Delivery.php
+++ Laravel/app/Clothing_Delivery.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Material_Clothing_Delivery;
 
 class Clothing_Delivery extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function Material_Clothing_Delivery()
+    {
+        return $this->hasMany(\App\Material_Clothing_Delivery::class);
+    }
 }
--- Laravel/app/Ticket_History.php
+++ Laravel/app/Ticket_History.php
@@ -3,8 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Email;
+use App\Ticket;
+use App\Action;
+use App\User;
 
 class Ticket_History extends Model
 {
-    //
+    public function email()
+    {
+        return $this->belongsTo(Email::class);
+    }
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    public function actions()
+    {
+        return $this->hasMany(\App\Action::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
