0b62dfdc8dc7366cc69688949f7653736c7c00f0
Ricardo Ferreira <rsfneg@gmail.com>
2023-12-13 14:22:40 +0000

    Auto stash before merge of "dev" and "origin/dev"
    added partner_contact model and seeder. missing relations

--- Laravel/database/seeds/DatabaseSeeder.php
+++ Laravel/database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(PartnerSeeder::class);
+        $this->call(PartnerContactSeeder::class);
 
     }
 }
---
+++ Laravel/database/seeds/PartnerContactSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PartnerContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Seed 1
+        DB::table('partner_contacts')->insert([
+            'contact' => '+351 229 570 000',
+            'partner_id' => 1,
+        ]);
+
+        // Seed 2
+        DB::table('partner_contacts')->insert([
+            'contact' => '+334 72 18 16 00',
+            'partner_id' => 2,
+        ]);
+
+        // Seed 3
+        DB::table('partner_contacts')->insert([
+            'contact' => '+330 239 792 100',
+            'partner_id' => 3,
+        ]);
+
+        // Seed 4
+        DB::table('partner_contacts')->insert([
+            'contact' => '+351 256 200 200',
+            'partner_id' => 4,
+        ]);
+
+        // Seed 5
+        DB::table('partner_contacts')->insert([
+            'contact' => '+440 223 744 000',
+            'partner_id' => 5,
+        ]);
+    }
+}
