fd75dbc6048ef3bd6daed7e4a5d14f38800b051d
Bernardo Teixeira <btteixeira.bt@gmail.com>
2023-12-13 22:33:53 +0000

    changed welcome page to show main

--- Laravel/resources/views/master/footer.blade.php
+++ Laravel/resources/views/master/footer.blade.php
@@ -1,3 +1,13 @@
 <footer>
-    <p align="center">This is a footer</p>
+    <p>This is a footer</p>
 </footer>
+
+
+<style>
+    p {
+        position: absolute;
+        bottom: 0;
+        text-align: center;
+        width: 100%;
+    }
+</style>
