fa8d2be994aaf456f6b3027ef48a42662090bda0
Ricardo Ferreira <ricardo.ferreira.t0123185@edu.atec.pt>
2023-12-13 20:27:56 +0000

    ticketHistory model+seeder+seeds. missing relations

--- Laravel/database/seeds/DatabaseSeeder.php
+++ Laravel/database/seeds/DatabaseSeeder.php
@@ -29,6 +29,7 @@ class DatabaseSeeder extends Seeder
         $this->call(TicketSeeder::class);
         $this->call(TechnicianTicketSeeder::class);
         $this->call(CommentSeeder::class);
+        $this->call(TicketHistorySeeder::class);
 
 
     }
---
+++ Laravel/database/seeds/TicketHistorySeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TicketHistorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Seed 1
+        DB::table('ticket_histories')->insert([
+            'ticket_id' => 1,
+            'ticket_info' => 'Ticket created.',
+            'action_id' => 1,
+        ]);
+
+        // Seed 2
+        DB::table('ticket_histories')->insert([
+            'ticket_id' => 2,
+            'ticket_info' => 'Ticket created.',
+            'action_id' => 1,
+        ]);
+
+        // Seed 3
+        DB::table('ticket_histories')->insert([
+            'ticket_id' => 3,
+            'ticket_info' => 'Ticket created.',
+            'action_id' => 1,
+        ]);
+
+        // Seed 4
+        DB::table('ticket_histories')->insert([
+            'ticket_id' => 4,
+            'ticket_info' => 'Ticket created.',
+            'action_id' => 1,
+        ]);
+
+        // Seed 5
+        DB::table('ticket_histories')->insert([
+            'ticket_id' => 5,
+            'ticket_info' => 'Ticket created.',
+            'action_id' => 3,
+        ]);
+    }
+}
