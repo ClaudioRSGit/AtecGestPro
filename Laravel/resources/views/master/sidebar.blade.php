4a884c5be17838a57fd74d6aff9bab6faf322028
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2023-12-15 19:14:46 +0000

    Users route created on sidebar

--- Laravel/resources/views/master/sidebar.blade.php
+++ Laravel/resources/views/master/sidebar.blade.php
@@ -17,7 +17,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path fill="#116fdc" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
             &nbsp
             &nbsp
-            <a href="#{{-- route('users') --}}">Utilizadores</a>
+            <a href="{{ route('users.index') }}">Utilizadores</a>
         </div>
 
         <div class="tickets">
