594f93e5ebc6bc0284690940e3e56187d674b6ad
Claudio <claudio.silva.t0123173@edu.atec.pt>
2023-12-15 20:16:00 +0000

    Students crud updated

---
+++ Laravel/resources/views/students/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Students List</h1>
+
+        <a href="{{ route('students.create') }}" class="btn btn-primary mb-3">Add Student</a>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($students as $student)
+                    <tr>
+                        <th scope="row">{{ $student->id }}</th>
+                        <td>{{ $student->name }}</td>
+                        <td>
+                            <a href="{{ route('students.show', $student->id) }}" class="btn btn-info">View</a>
+                            <a href="{{ route('students.edit', $student->id) }}" class="btn btn-warning">Edit</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
