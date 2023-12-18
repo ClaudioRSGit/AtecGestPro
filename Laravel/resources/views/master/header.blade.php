76bff55783856957d5b42df19d6bde77748d6978
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2023-12-18 23:38:50 +0000

    Updated navbar structure

--- Laravel/resources/views/master/header.blade.php
+++ Laravel/resources/views/master/header.blade.php
@@ -1,36 +1,32 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link mb-2">
+                <img src="https://cdn.icon-icons.com/icons2/2518/PNG/512/menu_icon_151204.png" alt="Menu" style="width: 25px; height: 25px;">
+            </a>
+        </li>
+    </ul>
+    <h5>Bem vindo, Fernando Almeida!</h5>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
+        <ul class="navbar-nav ml-auto">
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                    Dropdown
+                <a class="nav-link" href="#" id="notificacoesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="https://static.vecteezy.com/system/resources/previews/010/366/202/original/bell-icon-transparent-notification-free-png.png" alt="Sininho" style="width: 30px; height: 30px; margin-right: 5px;">
                 </a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    <a href="#{{-- route('clothing.index') --}}">Vestuário</a><
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="notificacoesDropdown">
+                    <a class="dropdown-item" href="#">Notificação 1</a>
+                    <a class="dropdown-item" href="#">Notificação 2</a>
                 </div>
             </li>
-            <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
+            <li class="nav-item dropdown">
+                <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="https://pbs.twimg.com/profile_images/1087303987307728898/yyr4CwNs_400x400.jpg" alt="Imagem Dropdown" class="rounded-circle" style="width: 30px; height: 30px; margin-right: 5px;">
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Perfil</a>
+                    <a class="dropdown-item" href="#">Sair</a>
+                </div>
             </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
     </div>
 </nav>
