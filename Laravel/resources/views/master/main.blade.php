42c29d8d59befd80520f601e22b3f2ca09484a9c
Claudio <claudio.silva.t0123173@edu.atec.pt>
2023-12-18 22:40:15 +0000

    Name and icon changed

--- Laravel/resources/views/master/main.blade.php
+++ Laravel/resources/views/master/main.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Project</title>
+    <title>AtecGest Pro</title>
     {{-- STYLE SECTION --}}
     <link rel="stylesheet" href="{!! asset('css/app.css') !!}" media="all" type="text/css" />
     <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/9/91/Gear-icon-blue-white-background.png" type="image/x-icon">
