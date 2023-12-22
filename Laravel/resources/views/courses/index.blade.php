12a9a4e0e3a76d3e78012bd1de3792a5198a2a45
Claudio <claudio.silva.t0123173@edu.atec.pt>
2023-12-22 00:07:01 +0000

    bugfixes

--- Laravel/resources/views/courses/index.blade.php
+++ Laravel/resources/views/courses/index.blade.php
@@ -81,17 +81,4 @@
             }
         });
     </script>
-    <style>
-        body::before {
-            content: '';
-            position: absolute;
-            top: 1%;
-            right: 0%;
-            bottom: 0%;
-            left: 50%;
-            position: fixed;
-            background-image: radial-gradient(circle, rgba(17, 111, 220, 0.1), rgba(120, 143, 228, 0.2), rgba(173, 177, 237, 0.1), rgba(217, 215, 246, 0), rgba(255, 255, 255, 0.1));
-            z-index: -1;
-        }
-    </style>
 @endsection
