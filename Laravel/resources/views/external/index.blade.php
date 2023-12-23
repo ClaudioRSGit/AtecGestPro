fffcf3da2a2abc15246e309deff695e0d3be9f4a
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2023-12-23 16:38:14 +0000

    Logic to show the Trainings of a specific Partner implemented

--- Laravel/resources/views/external/index.blade.php
+++ Laravel/resources/views/external/index.blade.php
@@ -28,7 +28,7 @@
                     </thead>
                     <tbody>
                         @foreach ($partner_Trainings_Users as $partner_Trainings_User)
-                            <tr>
+                            <tr class="partner_{{ $partner_Trainings_User->partner_id }}" style="display: none;">
                                 <td>{{ optional($partner_Trainings_User->partner)->name }}</td>
                                 <td>{{ optional($partner_Trainings_User->partner)->address }}</td>
                                 <td>{{ optional($partner_Trainings_User->user)->name }}</td>
@@ -80,8 +80,15 @@
                                 <td>{{ $partner->address }}</td>
                                 <td>
                                     <div class="dropdown">
-                                        <button class="btn btn-secondary dropdown-toggle" type="button" id="contactDropdown{{ $partner->id }}" data-toggle="dropdown">
-                                            View
+                                        <button class="btn btn-secondary dropdown-toggle" type="button"
+                                            id="contactDropdown{{ $partner->id }}" data-toggle="dropdown">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                                                <path
+                                                    d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                                            </svg>
                                         </button>
                                         <div class="dropdown-menu" aria-labelledby="contactDropdown{{ $partner->id }}">
                                             @foreach ($partner->partnerContacts as $contact)
@@ -90,7 +97,10 @@
                                         </div>
                                     </div>
                                 </td>
-                                <td><button>View</button></td>
+                                <td>
+                                    <button class="btn btn-info btn-sm"
+                                        onclick="filterTrainingsTable({{ $partner->id }})">View</button>
+                                </td>
                             </tr>
                         @endforeach
                     </tbody>
@@ -107,6 +117,31 @@
         function showTable(showId, hideId) {
             document.getElementById(showId).style.display = 'block';
             document.getElementById(hideId).style.display = 'none';
+
+            if (showId === 'trainingsTable') {
+                var trainingsTable = document.getElementById(showId);
+                var rows = trainingsTable.getElementsByTagName('tr');
+                for (var i = 0; i < rows.length; i++) {
+                    rows[i].style.display = '';
+                }
+            }
+        }
+
+        function filterTrainingsTable(partnerId) {
+            document.getElementById('partnersTable').style.display = 'none';
+
+            var trainingsTable = document.getElementById('trainingsTable');
+            trainingsTable.style.display = 'block';
+
+            var rows = trainingsTable.getElementsByTagName('tr');
+            for (var i = 0; i < rows.length; i++) {
+                rows[i].style.display = 'none';
+            }
+
+            var partnerRows = document.getElementsByClassName('partner_' + partnerId);
+            for (var x = 0; x < partnerRows.length; x++) {
+                partnerRows[x].style.display = '';
+            }
         }
     </script>
 @endsection
