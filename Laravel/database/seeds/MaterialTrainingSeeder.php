e72743ee40ff1a17a9a330e661de3ca9448e8ef6
Ricardo Ferreira <ricardo.ferreira.t0123185@edu.atec.pt>
2023-12-29 02:02:53 +0000

    changes to MaterialTrainingSeeder

--- Laravel/database/seeds/MaterialTrainingSeeder.php
+++ Laravel/database/seeds/MaterialTrainingSeeder.php
@@ -14,7 +14,7 @@ class MaterialTrainingSeeder extends Seeder
     {
         // Seed 1
         DB::table('material__trainings')->insert([
-            'partner__trainings__user_id' => 1,
+            'partner__trainings__user_id' => 5,
             'material_id' => 4,
             'quantity' => 1,
         ]);
