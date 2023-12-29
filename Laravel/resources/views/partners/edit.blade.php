e2ffaa1b9c26bd38d2d052ca016b7fe6bc9ecca0
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2023-12-29 22:45:36 +0000

    Implemented logic to dynamically add the delete contact fields button on the Partner Edit Contact form

--- Laravel/resources/views/partners/edit.blade.php
+++ Laravel/resources/views/partners/edit.blade.php
@@ -31,7 +31,8 @@
                     <div class="mb-3">
                         <div class="d-flex justify-content-between align-items-center">
                             <label for="contacts" class="form-label">Contatos:</label>
-                            <button type="button" class="btn btn-primary" onclick="addContactFields()">Novo Contato</button>
+                            <button type="button" class="btn btn-primary" onclick="addContactFields()">Novo
+                                Contato</button>
                         </div>
                         <div id="contacts-container">
                             @foreach ($partner->partnerContacts as $contact)
@@ -41,9 +42,13 @@
                                         value="{{ $contact->description }}" placeholder="Descrição">
                                     <input type="text" class="form-control" name="existing_contact_values[]"
                                         value="{{ $contact->contact }}" placeholder="Contato">
-                                        <button type="button" class="btn" onclick="removeContact(this)">
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path fill="#116fdc" d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>
-                                        </button>
+                                    <button type="button" class="btn" onclick="removeContact(this)">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14"
+                                            viewBox="0 0 448 512">
+                                            <path fill="#116fdc"
+                                                d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" />
+                                        </svg>
+                                    </button>
                                 </div>
                             @endforeach
                         </div>
@@ -112,17 +117,27 @@
             inputValue.name = 'new_contact_values[]';
             inputValue.placeholder = 'Contato';
 
+            var removeButton = document.createElement('button');
+            removeButton.type = 'button';
+            removeButton.classList.add('btn');
+            removeButton.innerHTML =
+                '<svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path fill="#116fdc" d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>';
+            removeButton.addEventListener('click', function() {
+                newContactGroup.remove();
+            });
+
             newContactGroup.appendChild(inputId);
             newContactGroup.appendChild(inputDescription);
             newContactGroup.appendChild(inputValue);
+            newContactGroup.appendChild(removeButton);
 
             contactsContainer.appendChild(newContactGroup);
         }
 
         function removeContact(button) {
-        var contactGroup = button.closest('.contact-group');
+            var contactGroup = button.closest('.contact-group');
 
-        contactGroup.remove();
-    }
+            contactGroup.remove();
+        }
     </script>
 @endsection
