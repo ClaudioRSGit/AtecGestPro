4f163811f3854f706e68e2d36fbfd597614c7ed6
Ricardo Ferreira <ricardo.ferreira.t0123185@edu.atec.pt>
2024-01-10 01:07:55 +0000

    wip

--- Laravel/app/Http/Controllers/MaterialClothingDeliveryController.php
+++ Laravel/app/Http/Controllers/MaterialClothingDeliveryController.php
@@ -7,8 +7,10 @@ use Illuminate\Http\Request;
 use App\Material;
 use App\User;
 use App\Clothing_Delivery;
-use App\MaterialClothingDelivery; //ficar de olho
-use App\Material_Clothing_Assignment;
+use App\MaterialClothingDelivery;
+
+//ficar de olho
+use App\Material_clothes;
 use Illuminate\Support\Facades\DB;
 
 class MaterialClothingDeliveryController extends Controller
@@ -31,35 +33,63 @@ class MaterialClothingDeliveryController extends Controller
     public function create($id)
     {
 
+        $student = User::with('CourseClass')->find($id);
 
-       $clothing_assignment = Material::where('isClothing', 1)->get();
-        $materials = Material::all();
+//        dd($student->CourseClass->course_id);
+
+        if ($student->CourseClass->course_id == 5) {
+            $clothes = Material::where('name', "Bata")->get();
+        } elseif ($student->CourseClass->course_id ==4){
+            $clothes = Material::where('name', 'like', '%Botas%')->get();
+
+        } else {
+            $clothes = Material::where('isClothing', 1)->get();
+        }
+
+
+
+        //$clothes = Material::where('isClothing', 1)->get();
+
         $clothing_deliveries = Clothing_Delivery::all();
 
-        $student = User::find($id);
 
 
-        return view('material-clothing-delivery.create', compact( 'clothing_assignment', 'materials', 'clothing_deliveries', 'student'));
+
+        return view('material-clothing-delivery.create', compact('clothes',  'clothing_deliveries', 'student'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
 
 
+//        selectedClothing
+        //user_id
+        //additionalNotes
+        //selectedClothing
+        //gender
+        //size
+        //quantity
 
 
         $validatedData = $request->validate([
             'user_id' => 'required|integer|exists:users,id',
             'additionalNotes' => 'nullable|string',
             'selectedClothing' => 'required|array',
-            'selectedClothing.*' => 'integer|exists:materials,id',
+            'selectedClothing.*' => 'required|string',
+            'size' => 'required|array',
+            'size.*' => 'string',
+            'quantity' => 'required|array',
+            'quantity.*' => 'integer',
+            'gender' => 'required|string'
         ]);
+
+      //  dd($request->input('quantity'));
 
         $clothingDelivery = new Clothing_Delivery;
         $clothingDelivery->user_id = $validatedData['user_id'];
@@ -67,14 +97,49 @@ class MaterialClothingDeliveryController extends Controller
         $clothingDelivery->save();
 
         foreach ($validatedData['selectedClothing'] as $materialId) {
-            $materialClothingDelivery = new Material_Clothing_Delivery;
-            $materialClothingDelivery->clothing_delivery_id = $clothingDelivery->id;
-            $materialClothingDelivery->material_id = $materialId;
-            $materialClothingDelivery->save();
+
+            foreach ($validatedData['selectedClothing'] as $materialName) {
+
+                $size = $validatedData['size'][$materialName];
+                $quantity = $validatedData['quantity'][$materialName];
+
+                if ($validatedData['gender'] == "male") {
+                    $gender = 0;
+                } else {
+                    $gender = 1;
+                }
+
+                //         dd($gender);
+                $material = Material::where('name', $materialName)
+                    ->where('size', $size)
+                    ->where(function ($query) use ($gender) {
+                        $query->where('gender', $gender)
+                            ->orWhereNull('gender');
+                    })
+                    ->first();
+
+
+
+               // dd($quantity);
+                if ($material) {
+                   // dd($material->id);
+                    $materialClothingDelivery = new Material_Clothing_Delivery;
+                    $materialClothingDelivery->quantity = $quantity;
+                    $materialClothingDelivery->clothing_delivery_id = $clothingDelivery->id;
+                    $materialClothingDelivery->material_id = $material->id;
+                    $materialClothingDelivery->save();
+                    $material->quantity = $material->quantity - $quantity;
+                    $material->save();
+                } else {
+
+                    dd("Material not found: " . $materialName);
+                }
+            }
+
         }
 
 
-    return redirect()->route('clothing.index')->with('success', 'Material inserido com sucesso!');
+        return redirect()->route('clothing.index')->with('success', 'Material inserido com sucesso!');
 
 
     }
@@ -82,7 +147,7 @@ class MaterialClothingDeliveryController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Material_Clothing_Delivery  $material_Clothing_Delivery
+     * @param \App\Material_Clothing_Delivery $material_Clothing_Delivery
      * @return \Illuminate\Http\Response
      */
     public function show(Material_Clothing_Delivery $material_Clothing_Delivery)
@@ -93,7 +158,7 @@ class MaterialClothingDeliveryController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Material_Clothing_Delivery  $material_Clothing_Delivery
+     * @param \App\Material_Clothing_Delivery $material_Clothing_Delivery
      * @return \Illuminate\Http\Response
      */
     public function edit(Material_Clothing_Delivery $material_Clothing_Delivery)
@@ -104,8 +169,8 @@ class MaterialClothingDeliveryController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Material_Clothing_Delivery  $material_Clothing_Delivery
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Material_Clothing_Delivery $material_Clothing_Delivery
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Material_Clothing_Delivery $material_Clothing_Delivery)
@@ -116,7 +181,7 @@ class MaterialClothingDeliveryController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Material_Clothing_Delivery  $material_Clothing_Delivery
+     * @param \App\Material_Clothing_Delivery $material_Clothing_Delivery
      * @return \Illuminate\Http\Response
      */
     public function destroy(Material_Clothing_Delivery $material_Clothing_Delivery)
--- Laravel/database/seeds/MaterialSeeder.php
+++ Laravel/database/seeds/MaterialSeeder.php
@@ -25,19 +25,7 @@ class MaterialSeeder extends Seeder
             'size' => null,
             'role' => null,
         ]);
-        // Seed 2
-        DB::table('materials')->insert([
-            'name' => 'Bata',
-            'description' => 'Bata laboratorios',
-            'isInternal' => 1,
-            'quantity' => 10,
-            'aquisition_date' => '2020-01-09 21:04:24',
-            'supplier' => 'Fardas e Companhia',
-            'isClothing' => 1,
-            'gender' => 1,
-            'size' => 'M',
-            'role' => 3,
-        ]);
+
         // Seed 3
         DB::table('materials')->insert([
             'name' => 'Lampada',
@@ -86,9 +74,285 @@ class MaterialSeeder extends Seeder
             'aquisition_date' => null,
             'supplier' => null,
             'isClothing' => 1,
-            'gender' => 0,
+            'gender' => null,
             'size' => '42',
             'role' => 3,
         ]);
+
+
+        DB::table('materials')->insert([
+            'name' => 'T-shirt',
+            'description' => 'T-shirt',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => '',
+            'isClothing' => 1,
+            'gender' => 1,
+            'size' => 'S',
+            'role' => 3,
+        ]);
+
+        DB::table('materials')->insert([
+            'name' => 'T-shirt',
+            'description' => 'T-shirt',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => '',
+            'isClothing' => 1,
+            'gender' => 1,
+            'size' => 'M',
+            'role' => 3,
+        ]);
+
+        DB::table('materials')->insert([
+            'name' => 'T-shirt',
+            'description' => 'T-shirt',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => '',
+            'isClothing' => 1,
+            'gender' => 1,
+            'size' => 'L',
+            'role' => 3,
+        ]);
+
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 0,
+            'size' => 'M',
+            'role' => 3,
+        ]);
+
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 0,
+            'size' => 'S',
+            'role' => 3,
+        ]);
+
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 0,
+            'size' => 'L',
+            'role' => 3,
+        ]);
+
+        // Seed 2
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 1,
+            'size' => 'M',
+            'role' => 3,
+        ]);
+
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 1,
+            'size' => 'S',
+            'role' => 3,
+        ]);
+
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 1,
+            'size' => 'L',
+            'role' => 3,
+        ]);
+
+        // Seed 3
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 0,
+            'size' => 'XS',
+            'role' => 3,
+        ]);
+
+// Seed 4
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 0,
+            'size' => 'XL',
+            'role' => 3,
+        ]);
+
+// Seed 5
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 1,
+            'size' => 'XXL',
+            'role' => 3,
+        ]);
+
+// Seed 6
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 1,
+            'size' => 'XXXL',
+            'role' => 3,
+        ]);
+
+// Seed 7
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 0,
+            'size' => 'XXXXL',
+            'role' => 3,
+        ]);
+
+// Seed 8
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 0,
+            'size' => 'S',
+            'role' => 3,
+        ]);
+
+// Seed 9
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 1,
+            'size' => 'S',
+            'role' => 3,
+        ]);
+
+// Seed 10
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 0,
+            'size' => 'M',
+            'role' => 3,
+        ]);
+
+// Seed 11
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 1,
+            'size' => 'M',
+            'role' => 3,
+        ]);
+
+// Seed 12
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 0,
+            'size' => 'L',
+            'role' => 3,
+        ]);
+
+// Seed 13
+        DB::table('materials')->insert([
+            'name' => 'Bata',
+            'description' => 'Bata laboratorios',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'aquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 1,
+            'size' => 'L',
+            'role' => 3,
+        ]);
+
+
+
     }
 }
