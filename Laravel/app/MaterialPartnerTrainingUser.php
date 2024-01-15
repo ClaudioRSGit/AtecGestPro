aaa857c4d08b02792228fdfdcec5cdd81a90fc8c
Ricardo Ferreira <ricardo.ferreira.t0123185@edu.atec.pt>
2024-01-15 21:02:03 +0000

    saving updated_at

--- Laravel/app/MaterialPartnerTrainingUser.php
+++ Laravel/app/MaterialPartnerTrainingUser.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MaterialPartnerTrainingUser extends Model
 {
+    use softDeletes;
     protected $fillable = [
         'material_id', 'partner_training_user_id', 'quantity'
     ];
