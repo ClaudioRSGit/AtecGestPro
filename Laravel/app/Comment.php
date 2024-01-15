2d58d45f2396db4a33bd6416f8daf46f043a913c
Ricardo Ferreira <ricardo.ferreira.t0123185@edu.atec.pt>
2024-01-15 22:08:24 +0000

    teste

--- Laravel/app/Comment.php
+++ Laravel/app/Comment.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
+    use softDeletes;
     public function ticket()
     {
         return $this->belongsTo(Ticket::class);
