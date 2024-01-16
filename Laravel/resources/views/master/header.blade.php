5ec6e326e01e692f8e55d7c60055df5ba8329c9e
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2024-01-16 15:43:45 +0000

    Updated the header so that the profile button appears if the logged in user is a technician or admin

--- Laravel/resources/views/master/header.blade.php
+++ Laravel/resources/views/master/header.blade.php
@@ -2,7 +2,8 @@
     <ul class="navbar-nav">
         <li class="nav-item">
             <a class="nav-link mb-2">
-                <img src="https://cdn.icon-icons.com/icons2/2518/PNG/512/menu_icon_151204.png" alt="Menu" style="width: 25px; height: 25px;">
+                <img src="https://cdn.icon-icons.com/icons2/2518/PNG/512/menu_icon_151204.png" alt="Menu"
+                    style="width: 25px; height: 25px;">
             </a>
         </li>
     </ul>
@@ -10,8 +11,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item dropdown">
-                <a class="nav-link" href="#" id="notificacoesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img src="https://static.vecteezy.com/system/resources/previews/010/366/202/original/bell-icon-transparent-notification-free-png.png" alt="Sininho" style="width: 30px; height: 30px; margin-right: 5px;">
+                <a class="nav-link" href="#" id="notificacoesDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    <img src="https://static.vecteezy.com/system/resources/previews/010/366/202/original/bell-icon-transparent-notification-free-png.png"
+                        alt="Sininho" style="width: 30px; height: 30px; margin-right: 5px;">
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="notificacoesDropdown">
                     <a class="dropdown-item" href="#">Notificação 1</a>
@@ -19,12 +22,18 @@
                 </div>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img src="https://pbs.twimg.com/profile_images/1087303987307728898/yyr4CwNs_400x400.jpg" alt="Imagem Dropdown" class="rounded-circle" style="width: 30px; height: 30px; margin-right: 5px;">
+                <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    <img src="https://pbs.twimg.com/profile_images/1087303987307728898/yyr4CwNs_400x400.jpg"
+                        alt="Imagem Dropdown" class="rounded-circle"
+                        style="width: 30px; height: 30px; margin-right: 5px;">
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Perfil</a>
-                    <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sair</a>
+                    @if (Auth::user()->hasRole('admin') || Auth::user()->hasRole('tecnico'))
+                        <a class="dropdown-item" href="#">Perfil</a>
+                    @endif
+                    <a class="dropdown-item" href="#"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sair</a>
                 </div>
             </li>
         </ul>
