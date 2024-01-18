5f959e68326f034847df4e1d1570edb6fca906cd
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2024-01-18 13:03:25 +0000

    Updated the Partner create form to always have a contact insertion field

--- Laravel/resources/views/partners/create.blade.php
+++ Laravel/resources/views/partners/create.blade.php
@@ -30,14 +30,14 @@
                         <div class="d-flex justify-content-between align-items-center">
                             <label for="contacts" class="form-label">Contatos:</label>
                             <button type="button" class="btn btn-primary" onclick="addContactFields()">Novo
-                                Contato</button>
+                                Contacto</button>
                         </div>
                         <div id="contacts-container">
                             <div class="contact-group mb-3">
                                 <input type="text" class="form-control" name="contact_description[]"
                                     placeholder="Descrição">
-                                <input type="text" class="form-control" name="contact_value[]" placeholder="Contato">
-                                <button type="button" class="btn" onclick="removeContact(this)">
+                                <input type="text" class="form-control" name="contact_value[]" placeholder="Contacto">
+                                <button type="button" class="btn remove-contact" onclick="removeContact(this)">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14"
                                         viewBox="0 0 448 512">
                                         <path fill="#116fdc"
@@ -64,19 +64,40 @@
     </div>
 
     <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            updateRemoveButtonState();
+
+            document.querySelector('#contacts-container').addEventListener('input', function(event) {
+                if (event.target.tagName === 'INPUT' && event.target.name.startsWith('contact_')) {
+                    updateRemoveButtonState();
+                }
+            });
+        });
+
         function validateForm() {
+            const firstContactGroup = document.querySelector('.contact-group');
+            const firstDescriptionInput = firstContactGroup.querySelector('input[name^="contact_description"]');
+            const firstValueInput = firstContactGroup.querySelector('input[name^="contact_value"]');
+
+            if (firstDescriptionInput.value.trim() === '' && firstValueInput.value.trim() === '') {
+                firstContactGroup.remove();
+            }
+
             removeEmptyContactFields();
         }
+
 
         function removeEmptyContactFields() {
             const contactGroups = document.querySelectorAll('.contact-group');
 
-            for (let i = contactGroups.length - 1; i >= 0; i--) {
-                const contactDescription = contactGroups[i].querySelector('input[name^="contact_description"]');
-                const contactValue = contactGroups[i].querySelector('input[name^="contact_value"]');
+            if (contactGroups.length > 1) {
+                for (let i = contactGroups.length - 1; i >= 0; i--) {
+                    const contactDescription = contactGroups[i].querySelector('input[name^="contact_description"]');
+                    const contactValue = contactGroups[i].querySelector('input[name^="contact_value"]');
 
-                if (contactDescription.value.trim() === '' || contactValue.value.trim() === '') {
-                    contactDescription.parentNode.remove();
+                    if (contactDescription.value.trim() === '' || contactValue.value.trim() === '') {
+                        contactGroups[i].remove();
+                    }
                 }
             }
         }
@@ -105,33 +126,60 @@
                 inputValue.type = 'text';
                 inputValue.classList.add('form-control');
                 inputValue.name = 'contact_value[]';
-                inputValue.placeholder = 'Contato';
+                inputValue.placeholder = 'Contacto';
 
                 const removeButton = document.createElement('button');
                 removeButton.type = 'button';
-                removeButton.classList.add('btn');
+                removeButton.classList.add('btn', 'remove-contact');
                 removeButton.innerHTML =
                     '<svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path fill="#116fdc" d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>';
                 removeButton.addEventListener('click', function() {
-                    newContactGroup.remove();
+                    if (contactGroups.length > 1 || (contactGroups.length === 1 && (inputDescription.value
+                            .trim() !== '' || inputValue.value.trim() !== ''))) {
+                        newContactGroup.remove();
+                        updateRemoveButtonState();
+                    }
                 });
 
                 newContactGroup.appendChild(inputDescription);
                 newContactGroup.appendChild(inputValue);
                 contactsContainer.appendChild(newContactGroup);
                 newContactGroup.appendChild(removeButton);
+                updateRemoveButtonState();
 
                 inputDescription.value = '';
                 inputValue.value = '';
             } else {
-                alert('Preencha todos os campos dos contatos anteriores!');
+                alert('Preencha todos os campos dos contactos anteriores!');
             }
         }
 
         function removeContact(button) {
             const contactGroup = button.closest('.contact-group');
-
+            const contactsContainer = document.getElementById('contacts-container');
             contactGroup.remove();
+            updateRemoveButtonState();
+
+            if (document.querySelectorAll('.contact-group').length === 0) {
+                addContactFields();
+            }
+        }
+
+        function updateRemoveButtonState() {
+            const contactGroups = document.querySelectorAll('.contact-group');
+            const removeButtons = document.querySelectorAll('.remove-contact');
+
+            if (contactGroups.length === 1) {
+                const contactDescription = contactGroups[0].querySelector('input[name^="contact_description"]');
+                const contactValue = contactGroups[0].querySelector('input[name^="contact_value"]');
+                removeButtons.forEach(function(button) {
+                    button.disabled = (contactDescription.value.trim() === '' && contactValue.value.trim() === '');
+                });
+            } else {
+                removeButtons.forEach(function(button) {
+                    button.disabled = false;
+                });
+            }
         }
     </script>
 @endsection
