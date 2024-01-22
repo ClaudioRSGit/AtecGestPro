0322395bc22393b32a92a6332cc4284967d13ed6
Wilson Miranda <wilson.miranda.t0123188@atec.edu.pt>
2024-01-22 11:11:19 +0000

    Revert "minor fixes"
    This reverts commit 397e9f97b5287088d442a63e9a52846ab1d00dd4.

---
+++ Laravel/app/Http/Controllers/ExcelImportController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Imports\UserImportClass;
+use App\Imports\StudentImportClass;
+use App\User;
+
+class ExcelImportController extends Controller
+{
+    public function index()
+    {
+        return view('excel.importStudents');
+    }
+
+    public function importUsers(Request $request)
+    {
+        // Validate the uploaded file
+        $request->validate([
+            'file' => 'required|mimes:xlsx,xls',
+        ]);
+
+        // Get the uploaded file
+        $file = $request->file('file');
+
+        // Process the Excel file
+        Excel::import(new UserImportClass, $file);
+
+        $users = User::paginate(5);
+
+        if ($request->ajax()) {
+            return view('users.partials.index', ['users' => $users]);
+        }
+
+        return view('excel.excel-index', ['users' => $users]);
+    }
+
+
+
+    public function importStudents(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|mimes:xlsx,xls',
+        ]);
+
+        // Get the uploaded file
+        $file = $request->file('file');
+
+        Excel::import(new StudentImportClass, $file);
+
+        return view('excel.studentsSuccess');
+    }
+}
