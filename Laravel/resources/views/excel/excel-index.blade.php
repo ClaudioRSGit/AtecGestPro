397e9f97b5287088d442a63e9a52846ab1d00dd4
Wilson Miranda <wilson.miranda.t0123188@atec.edu.pt>
2024-01-22 11:10:34 +0000

    minor fixes

--- Laravel/resources/views/excel/excel-index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('master.main')
-
-@section('content')
-
-<h1>Utilizadores importados com sucesso!</h1>
-<a href="{{ route('users.index') }}" class="btn btn-primary">Voltar à página de utilizadores</a>
-
-@endsection
--- Laravel/resources/views/excel/studentsSuccess.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('master.main')
-
-@section('content')
-
-<h1>Alunos importados com sucesso!</h1>
-<a href="{{ route('course-classes.index') }}" class="btn btn-primary">Voltar à página de turmas</a>
-
-@endsection
