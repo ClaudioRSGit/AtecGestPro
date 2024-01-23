104c04d78f129bd79053580f4677a7bf2f02326c
Ricardo Ferreira <ricardo.ferreira.t0123185@edu.atec.pt>
2024-01-23 00:56:34 +0000

    fix outros tab to display only non docents

--- Laravel/resources/views/material-user/index.blade.php
+++ Laravel/resources/views/material-user/index.blade.php
@@ -103,6 +103,7 @@
                                                     <td>{{ $student->name }}</a></td>
                                                     <td>{{ $student->username }}</td>
                                                     <td>{{ $student->email }}</td>
+                                                    {{-- <td><input type="checkbox" class="no-propagate"></td> --}}
                                                     <td class="editDelete">
                                                         <div style="width: 40%">
                                                             <a href="{{ route('material-user.edit', $student->id) }}"
@@ -115,6 +116,7 @@
                                                             </a>
                                                         </div>
                                                         <div style="width: 40%">
+{{--  --}}
                                                         </div>
                                                     </td>
                                                 </tr>
@@ -135,11 +137,11 @@
 
 
             <div class="tab-pane fade" id="outros">
-                <div class=" d-flex justify-content-between align-items-center mb-3 " style="gap: 1rem">
+                <div class="w-100 d-flex justify-content-between align-items-center mb-3" style="gap: 1rem">
                     <div class="search-container w-40">
                         <input type="text" id="search" class="form-control w-80" placeholder="Pesquisar Utilizador">
                     </div>
-                    <div class="">
+                    <div class="w-15">
                         <select class="form-control" id="filter">
                             <option value="all">Todos</option>
                             @foreach ($nonDocents as $nonDocent)
@@ -154,7 +156,7 @@
                         </select>
                     </div>
                 </div>
-                <table class="table ">
+                <table class="table">
                     <thead>
                     <tr>
                         <th>Nome</th>
@@ -164,24 +166,20 @@
                     </tr>
                     </thead>
                     <tbody class="customTableStyling">
-                    <tr class="filler"></tr>
-                    @foreach ($courseClass->students as $student)
-                        <tr class="customTableStyling"
-                            onclick="location.href='{{ route('material-user.create', $student->id) }}'">
-                            <td>{{ $student->name }}</td>
-                            <td>{{ $student->username }}</td>
-                            <td>{{ $student->email }}</td>
-                            <td class="editDelete">
-                                <div style="width: 40%">
-                                    <a href="{{ route('material-user.edit', $student->id) }}" class="mx-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16"
-                                             viewBox="0 0 512 512">
-                                            <path fill="#116fdc"
-                                                  d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/>
-                                        </svg>
-                                    </a>
-                                </div>
-                                <div style="width: 40%"></div>
+                    @foreach ($nonDocents as $nonDocent)
+                        <tr class="filler"
+                            onclick="location.href='{{ route('material-user.create', $nonDocent->id) }}'">
+                            <td>{{ $nonDocent->name }}</td>
+                            <td>{{ $nonDocent->username }}</td>
+                            <td>{{ $nonDocent->email }}</td>
+                            <td>
+                                <a href="{{ route('users.edit', $nonDocent->id) }}" class="mx-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16"
+                                         viewBox="0 0 512 512">
+                                        <path fill="#116fdc"
+                                              d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/>
+                                    </svg>
+                                </a>
                             </td>
                         </tr>
                         <tr class="filler"></tr>
