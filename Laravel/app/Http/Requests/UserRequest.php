129807576b44f5257a85f1586a881b48c66862b4
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2024-01-23 12:21:39 +0000

    Updated error messages in UserRequest

--- Laravel/app/Http/Requests/UserRequest.php
+++ Laravel/app/Http/Requests/UserRequest.php
@@ -24,10 +24,10 @@ class UserRequest extends FormRequest
     public function rules()
     {
         $rules = [
-            'name' => 'required|string|min:5|max:255',
+            'name' => 'required|string|min:3|max:200',
             'username' => 'required|string|min:5|max:20',
             'email' => 'required|email',
-            'contact' => 'required|min:9|max:20',
+            'contact' => 'required|min:9|max:20|regex:/^[\s\d()+-]+$/',
             'isStudent' => 'required',
             'isActive' => 'required',
             'course_class_id' => 'nullable',
@@ -50,12 +50,23 @@ class UserRequest extends FormRequest
     {
         return [
             'name.required' => 'O nome é obrigatório!',
-            'username.required' => 'O nome de usuário é obrigatório!',
+            'name.min' => 'O nome deve ter pelo menos 5 caracteres!',
+            'name.max' => 'O nome deve ter no máximo 200 caracteres!',
+
+            'username.required' => 'O nome de utilizador é obrigatório!',
+            'username.min' => 'O username deve ter pelo menos 5 caracteres!',
+            'username.max' => 'O username deve ter no máximo 20 caracteres!',
+
             'email.required' => 'O email é obrigatório!',
-            'email.email' => 'O email deve ser um endereço de email válido!',
+            'email.email' => 'Formato de email inválido!',
+
             'contact.required' => 'O contato é obrigatório!',
+            'contact.min' => 'O contato deve ter pelo menos 9 caracteres!',
+            'contact.max' => 'O contato deve ter no máximo 20 caracteres!',
+            'contact.regex' => 'Formato de contacto inválido!',
+
             'password.required' => 'A password é obrigatória!',
-            // 'password.?' => 'A Password deve ter pelo menos uma letra maiúscula, um caracter especial e sete caracteres!',
+            'password.regex' => 'A password deve ter pelo menos uma letra maiúscula, um caracter especial e sete caracteres!',
         ];
     }
 }
