2ae039990283c5fff6b5e67684f757d34fbbd2c1
Wilson Miranda <wilson.miranda.t0123188@atec.edu.pt>
2024-01-24 15:44:32 +0000

    Create dashboard MVC

---
+++ Laravel/database/factories/DashboardFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Dashboard;
+use Faker\Generator as Faker;
+
+$factory->define(Dashboard::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
