b68f80395371f3ec488ef74112b14416286709f2
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2024-01-24 17:28:15 +0000

    Trainings create form updated to show error messages

--- Laravel/resources/views/trainings/create.blade.php
+++ Laravel/resources/views/trainings/create.blade.php
@@ -10,16 +10,28 @@
             <div class="mb-3">
                 <label for="name" class="form-label">Nome da formação</label>
                 <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
+
+                @error('name')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="description" class="form-label">Descrição:</label>
                 <textarea class="form-control" id="description" name="description">{{ old('description') }}</textarea>
+
+                @error('description')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="category" class="form-label">Categoria:</label>
                 <textarea class="form-control" id="category" name="category">{{ old('category') }}</textarea>
+
+                @error('category')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
 
 
