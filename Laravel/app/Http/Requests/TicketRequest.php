d38f9990a858f1b027687bd96fc574428370aa8b
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2024-01-25 16:53:42 +0000

    TicketRequest  updated

--- Laravel/app/Http/Requests/TicketRequest.php
+++ Laravel/app/Http/Requests/TicketRequest.php
@@ -13,7 +13,7 @@ class TicketRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,30 @@ class TicketRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'user_id' => 'required|integer|exists:users,id',
+            'title' => 'required|string|min:2|max:50',
+            'description' => 'required|string|min:5|max:100',
+            'technician_id' => 'required|exists:users,id',
+            'dueByDate' => 'required|date',
+            'attachment' => 'sometimes|file|max:20480', // 20MB
+            'priority_id' => 'required|exists:ticket_priorities,id',
+            'category_id' => 'required|exists:ticket_categories,id',
+            'ticket_status_id' => 'required|integer|exists:ticket_statuses,id',
+            'ticket_priority_id' => 'required|integer|exists:ticket_priorities,id',
+            'ticket_category_id' => 'required|integer|exists:ticket_categories,id',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'O título é obrigatório!',
+            'title.min' => 'O título deve conter no mínimo 2 caracteres!',
+            'title.max' => 'O título deve conter no máximo 50 caracteres!',
+            'description.required' => 'A descrição é obrigatória!',
+            'description.min' => 'A descrição deve conter no mínimo 5 caracteres!',
+            'description.max' => 'A descrição deve conter no máximo 100 caracteres!',
+            'description.string' => 'Formato inválido',
         ];
     }
 }
