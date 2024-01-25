88b7b5469bd8e9be9237ba99d696f282a5b2071e
Wilson Miranda <wilson.miranda.t0123188@atec.edu.pt>
2024-01-25 21:49:11 +0000

    Add traffic chart to dashboard

--- Laravel/resources/views/dashboard/index.blade.php
+++ Laravel/resources/views/dashboard/index.blade.php
@@ -130,7 +130,17 @@
         </div>
     </div>
 
-
+    <script>
+        new Chartist.Line('#traffic-chart', {
+            labels: ['January', 'Februrary', 'March', 'April', 'May', 'June'],
+            series: [
+                [23000, 25000, 19000, 34000, 56000, 64000]
+            ]
+            }, {
+            low: 0,
+            showArea: true
+        });
+    </script>
 
 
 @endsection
