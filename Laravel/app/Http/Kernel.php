c15ba036a15d65c2d3bea90c05a6ce9e36d5d5bb
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2024-01-31 21:56:21 +0000

    checkFuncionarioEditPermission registed on Kernel

--- Laravel/app/Http/Kernel.php
+++ Laravel/app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkRole' => \App\Http\Middleware\CheckRole::class,
+        'checkFuncionarioEditPermission' => \App\Http\Middleware\CheckEditPermission::class,
     ];
 }
