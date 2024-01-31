bdddf9980a6730618794b31a29b8be5cb7aae7cf
Ricardo Ferreira <ricardo.ferreira.t0123185@edu.atec.pt>
2024-01-31 19:01:52 +0000

    last ticket tab saved in local storage

--- Laravel/resources/views/tickets/index.blade.php
+++ Laravel/resources/views/tickets/index.blade.php
@@ -342,4 +342,18 @@
             });
         }, 2000);
     </script>
+
+    <script>
+        $(function() {
+            $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+                localStorage.setItem('lastTab', $(this).attr('href'));
+            });
+
+            let lastTab = localStorage.getItem('lastTab');
+            if (lastTab) {
+                $('[href="' + lastTab + '"]').tab('show');
+            }
+        });
+    </script>
+
     @endsection
