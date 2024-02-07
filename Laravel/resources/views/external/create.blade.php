43fc0a427ba51f48ff6a83d394b127455c1d29fa
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2024-02-07 20:19:37 +0000

    external index script logic relocated to the corresponding script file

--- Laravel/resources/views/external/create.blade.php
+++ Laravel/resources/views/external/create.blade.php
@@ -4,7 +4,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
 
     <link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
-
 @endsection
 
 @section('content')
@@ -18,11 +17,11 @@
     <div class="w-100">
 
         @error('start_date')
-        <div class="alert alert-danger success-alert">{{ $message }}</div>
+            <div class="alert alert-danger error-alert">{{ $message }}</div>
         @enderror
 
         @error('end_date')
-        <div class="alert alert-danger success-alert">{{ $message }}</div>
+            <div class="alert alert-danger error-alert">{{ $message }}</div>
         @enderror
 
         <h1>Agendar formação de mercado</h1>
@@ -33,7 +32,7 @@
                 <div class="training">
                     <label for="training_id">Formação:</label>
                     <select class="form-control" id="training_id" name="training_id" required>
-                        @foreach($trainings as $training)
+                        @foreach ($trainings as $training)
                             <option value="{{ $training->id }}">{{ $training->name }}</option>
                         @endforeach
                     </select>
@@ -41,21 +40,19 @@
                 <div class="partner">
                     <label for="partner_id">Parceiro:</label>
                     <select class="form-control" id="partner_id" name="partner_id" required>
-                        @foreach($partners as $partner)
-                            <option value="{{ $partner->id }}"
-                                    data-address="{{ $partner->address }}">{{ $partner->name }}</option>
+                        @foreach ($partners as $partner)
+                            <option value="{{ $partner->id }}" data-address="{{ $partner->address }}">{{ $partner->name }}
+                            </option>
                         @endforeach
                     </select>
                 </div>
                 <div class="technician">
                     <label for="user_id">Técnico:</label>
                     <select class="form-control" id="user_id" name="user_id" required>
-                        @foreach($users as $user)
-
-                            @if( $user->role_id == 4)
+                        @foreach ($users as $user)
+                            @if ($user->role_id == 4)
                                 <option value="{{ $user->id }}">{{ $user->name }}</option>
                             @endif
-
                         @endforeach
                     </select>
                 </div>
@@ -66,45 +63,45 @@
                 <div class="materials">
                     <table class="table bg-white">
                         <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Descrição</th>
-                            <th>Quantidade</th>
-                            <th>Selecionar</th>
-                        </tr>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Descrição</th>
+                                <th>Quantidade</th>
+                                <th>Selecionar</th>
+                            </tr>
                         </thead>
                         <tbody class="customTableStyling">
-                        <tr class="filler"></tr>
-                        @foreach($materials as $material)
-                            <tr>
-                                <td>{{ $material->name }}</td>
-                                <td>{{ $material->description }}</td>
-                                <td class="pl-4">
-                                    <input type="number" name="material_quantities[{{ $material->id }}]" value="1"
-                                           min="1" max="{{ $material->quantity }}"
-                                           @if($material->quantity == 0) disabled @endif>
-                                </td>
-                                <td class="pl-5">
-                                    <input type="checkbox" name="materials[]" value="{{ $material->id }}"
-                                           @if($material->quantity == 0) disabled @endif>
-                                </td>
-                            </tr>
                             <tr class="filler"></tr>
-                        @endforeach
+                            @foreach ($materials as $material)
+                                <tr>
+                                    <td>{{ $material->name }}</td>
+                                    <td>{{ $material->description }}</td>
+                                    <td class="pl-4">
+                                        <input type="number" name="material_quantities[{{ $material->id }}]"
+                                            value="1" min="1" max="{{ $material->quantity }}"
+                                            @if ($material->quantity == 0) disabled @endif>
+                                    </td>
+                                    <td class="pl-5">
+                                        <input type="checkbox" name="materials[]" value="{{ $material->id }}"
+                                            @if ($material->quantity == 0) disabled @endif>
+                                    </td>
+                                </tr>
+                                <tr class="filler"></tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
 
                 <div class="startCalendar">
                     <label for="start_date">Data de Início:</label>
-                    <input type="datetime-local" class="form-control flatpickr" id="start_date" name="start_date"
-                           required placeholder="Selecione a data de início">
+                    <input type="datetime-local" class="form-control flatpickr" id="start_date" name="start_date" required
+                        placeholder="Selecione a data de início">
                 </div>
 
                 <div class="endCalendar">
                     <label for="end_date">Data de Fim:</label>
-                    <input type="datetime-local" class="form-control flatpickr" id="end_date" name="end_date"
-                           required placeholder="Selecione a data de fim">
+                    <input type="datetime-local" class="form-control flatpickr" id="end_date" name="end_date" required
+                        placeholder="Selecione a data de fim">
                 </div>
 
                 <div class="btns">
@@ -139,7 +136,7 @@
             grid-gap: 1rem;
         }
 
-        .grid > div {
+        .grid>div {
             width: 100%;
         }
 
@@ -195,46 +192,11 @@
         .btns {
             grid-area: buttons;
         }
-
-
     </style>
-
 @endsection
 
 @section('scripts')
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <script src="https://npmcdn.com/flatpickr/dist/l10n/pt.js"></script>
-    <script>
-
-        jQuery(function () {
-            flatpickr("#start_date, #end_date", {
-                inline: true,
-                altInput: true,
-                altFormat: "F j, Y",
-                dateFormat: "Y-m-d",
-                minDate: "today",
-                locale: "pt"
-            });
-        });
-
-        document.addEventListener('DOMContentLoaded', function () {
-            var partnerDropdown = document.getElementById('partner_id');
-            var addressField = document.getElementById('address');
-
-            function setAddress() {
-                var selectedOption = partnerDropdown.options[partnerDropdown.selectedIndex];
-                addressField.value = selectedOption.getAttribute('data-address');
-            }
-
-            setAddress();
-
-            partnerDropdown.addEventListener('change', setAddress);
-        });
-
-        window.setTimeout(function () {
-            $(".success-alert").fadeTo(500, 0).slideUp(500, function () {
-                $(this).remove();
-            });
-        }, 2500);
-    </script>
+    <script type="module" src="{{ asset('js/external/create.js') }}"></script>
 @endsection
