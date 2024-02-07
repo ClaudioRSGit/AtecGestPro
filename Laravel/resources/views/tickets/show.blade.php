3b2042eeaba8bc2c0aa444b0e9a2f9bc75d9695a
Wilson Miranda <wilson.miranda.t0123188@atec.edu.pt>
2024-02-07 20:11:18 +0000

    refactor style on ticket page

--- Laravel/resources/views/tickets/show.blade.php
+++ Laravel/resources/views/tickets/show.blade.php
@@ -7,24 +7,23 @@
         <ul class="nav nav-tabs" id="myTab" role="tablist">
             <li class="nav-item" role="presentation">
                 <a class="nav-link active" id="ticket-details-tab" data-toggle="tab" href="#ticket-details" role="tab"
-                   aria-controls="ticket-details" aria-selected="true">Ticket Details</a>
+                    aria-controls="ticket-details" aria-selected="true">Ticket Details</a>
             </li>
             <li class="nav-item" role="presentation">
                 <a class="nav-link" id="ticket-history-tab" data-toggle="tab" href="#ticket-history" role="tab"
-                   aria-controls="ticket-history" aria-selected="false">Ticket History</a>
+                    aria-controls="ticket-history" aria-selected="false">Ticket History</a>
             </li>
         </ul>
 
         <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="ticket-details" role="tabpanel"
-                 aria-labelledby="ticket-details-tab">
+            <div class="tab-pane fade show active" id="ticket-details" role="tabpanel" aria-labelledby="ticket-details-tab">
 
                 <div class="row my-2">
                     <div class="col-md-9">
                         <div class="mb-3">
                             <label for="requester" class="form-label">Utilizador:</label>
                             <input type="text" class="form-control" id="requester" name="requester"
-                                   value="{{ $requester->name }}" disabled>
+                                value="{{ $requester->name }}" disabled>
                         </div>
                         <div class="mb-3">
                             <label for="title" class="form-label">Título:</label>
@@ -33,8 +32,7 @@
 
                         <div class="mb-3">
                             <label for="description" class="form-label">Descrição:</label>
-                            <textarea class="form-control" id="description"
-                                      disabled>{{ $ticket->description }}</textarea>
+                            <textarea class="form-control" id="description" disabled>{{ $ticket->description }}</textarea>
                         </div>
 
                         <div class="mb-2">
@@ -44,8 +42,8 @@
                         <div class="mb-3">
                             <label for="attachment" class="form-label">Anexo:</label>
                             <input type="text" class="form-control" id="attachment" value="{{ $ticket->attachment }}"
-                                   disabled style="border-radius: 5px;">
-                            @if($ticket->attachment)
+                                disabled style="border-radius: 5px;">
+                            @if ($ticket->attachment)
                                 <a href="{{ asset('storage/' . $ticket->attachment) }}" target="_blank">Ver Anexo</a>
                             @endif
                         </div>
@@ -65,7 +63,7 @@
                             <div class="mb-3">
                                 <label for="comments" class="form-label">Comentários:</label>
                                 @if ($ticket->comments->isNotEmpty())
-                                    @foreach($ticket->comments as $comment)
+                                    @foreach ($ticket->comments as $comment)
                                         <div class="card mb-2">
                                             <div class="card-body d-flex justify-content-between">
                                                 <div>
@@ -94,7 +92,7 @@
                         <div class="mb-3">
                             <label for="status" class="form-label">Estado:</label>
                             <input type="text" class="form-control"
-                                   value="{{ $ticket->ticketStatus->description ?? 'N/A' }}" disabled>
+                                value="{{ $ticket->ticketStatus->description ?? 'N/A' }}" disabled>
                         </div>
 
                         <div class="mb-3">
@@ -105,37 +103,30 @@
                         <div class="mb-3">
                             <label for="priority" class="form-label">Prioridade:</label>
                             <input type="text" class="form-control"
-                                   value="{{ $ticket->ticketPriority->description ?? 'N/A' }}" disabled>
+                                value="{{ $ticket->ticketPriority->description ?? 'N/A' }}" disabled>
                         </div>
 
                         <div class="mb-3">
                             <label for="category" class="form-label">Categoria:</label>
                             <input type="text" class="form-control"
-                                   value="{{ $ticket->ticketCategory->description ?? 'N/A' }}" disabled>
+                                value="{{ $ticket->ticketCategory->description ?? 'N/A' }}" disabled>
                         </div>
 
                         <div class="mb-3">
                             <label>Data Limite:</label>
-                            <input type="text" class="form-control" value="{{ $ticket->dueByDate ?? 'N/A' }}" disabled>
+                            <input type="text" class="form-control" value="{{ $ticket->dueByDate ?? 'N/A' }}"
+                                disabled>
                         </div>
 
-                        <div>
+                        <div class="mb-5">
                             <label>Histórico do Utilizador:</label>
-                            <div class="mb-5" id="histTickets">
+                            <div id="histTickets">
                                 <ul>
-                                    @foreach($userTickets as $userTicketId)
+                                    @foreach ($userTickets as $userTicketId)
                                         <li><a href="{{ route('tickets.show', $userTicketId) }}">Ticket
                                                 #{{ $userTicketId }}</a></li>
                                     @endforeach
                                 </ul>
-                            </div>
-                        </div>
-
-
-                        <div class="mb-5">
-                            <div class="mb-5">
-                                <label>Histórico do Ticket:</label>
-                                <a href="{{ route('ticket-histories.show', $ticket->id) }}">Ver Histórico</a>
                             </div>
                         </div>
                     </div>
@@ -143,28 +134,28 @@
             </div>
 
 
-                <div class="tab-pane fade" id="ticket-history" role="tabpanel" aria-labelledby="ticket-history-tab">
-                    <div class="my-2">
-                        <div class="card bg-primary">
+            <div class="tab-pane fade" id="ticket-history" role="tabpanel" aria-labelledby="ticket-history-tab">
+                <div class="my-2">
+                    <div class="card bg-primary">
 
-                            <div class="card-body ">
-                                @foreach($ticketHistories as $history)
-                                    <div class="card mb-3 shadow-lg">
-                                        <div class="card-header d-flex justify-content-center align-items-center">
-                                            <h5 class="mb-0">{{$history->action->description}}</h5>
-                                        </div>
-                                        <div class="card-body d-flex justify-content-center align-items-center">
-                                            <p class="card-title">{{$history->created_at}}</p>
-                                        </div>
-                                        <div class="card-body d-flex justify-content-center align-items-center">
-                                            <p>{!! nl2br(str_replace('.', ".\n", e($history->ticket_info))) !!}</p>
-                                        </div>
+                        <div class="card-body ">
+                            @foreach ($ticketHistories as $history)
+                                <div class="card mb-3 shadow-lg">
+                                    <div class="card-header d-flex justify-content-center align-items-center">
+                                        <h5 class="mb-0">{{ $history->action->description }}</h5>
                                     </div>
-                                @endforeach
-                            </div>
+                                    <div class="card-body d-flex justify-content-center align-items-center">
+                                        <p class="card-title">{{ $history->created_at }}</p>
+                                    </div>
+                                    <div class="card-body d-flex justify-content-center align-items-center">
+                                        <p>{!! nl2br(str_replace('.', ".\n", e($history->ticket_info))) !!}</p>
+                                    </div>
+                                </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
+            </div>
 
         </div>
 
@@ -175,12 +166,11 @@
         #histTickets {
             box-shadow: 1px 2px 1px 2px rgb(230, 229, 229);
             margin: 15px;
-            border: 1px solid #141313;
-            /* background-color: rgba(203, 234, 248, 0.3); */
             overflow-y: auto;
             overflow-x: auto;
-            height: 55px;
+            height: 200px;
+            scrollbar-width: none;
+            -ms-overflow-style: none;
         }
-
     </style>
 @endsection
