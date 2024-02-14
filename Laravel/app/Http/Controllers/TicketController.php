a01d09f082d5a501b0fb3b7f512fdc53ec8631fe
Bernardo Teixeira <btteixeira.bt@gmail.com>
2024-02-14 20:38:49 +0000

    changed 'x' button to red and fixed nofication content alignment

--- Laravel/app/Http/Controllers/TicketController.php
+++ Laravel/app/Http/Controllers/TicketController.php
@@ -238,7 +238,7 @@ class TicketController extends Controller
              }
 
              $notification = Notification::create([
-                 'description' => 'Ticket atribuido: #' . $ticket->id,
+                 'description' => 'Ticket atribuído: #' . $ticket->id,
                  'code' => 'TICKET',
                  'object_id' => $ticket->id,
              ]);
