5a16d6da580af6f521e5ce85b8f3542e6b328848
Wilson Miranda <wilson.miranda.t0123188@edu.atec.pt>
2024-02-14 20:02:56 +0000

    change 404 error page

---
+++ Laravel/resources/views/errors/404.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>404 Not Found</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+		 integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+       body{
+  margin:0;
+  padding:0;
+  font-family: 'Tomorrow', sans-serif;
+  height:100vh;
+background-image: linear-gradient(to top, #ffffff, #f2f2f2, #e6e6e6, #55a1d4, #0886da);
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  overflow:hidden;
+}
+.text{
+  position:absolute;
+  top:10%;
+  color:#fff;
+  text-align:center;
+}
+h1{
+  font-size:50px;
+}
+.star{
+  position:absolute;
+  width:2px;
+  height:2px;
+  background:#fff;
+  right:0;
+  animation:starTwinkle 3s infinite linear;
+}
+.astronaut img{
+  width:100px;
+  position:absolute;
+  top:55%;
+  animation:astronautFly 6s infinite linear;
+}
+@keyframes astronautFly{
+  0%{
+    left:-100px;
+  }
+  25%{
+    top:50%;
+    transform:rotate(30deg);
+  }
+  50%{
+    transform:rotate(45deg);
+    top:55%;
+  }
+  75%{
+    top:60%;
+    transform:rotate(30deg);
+  }
+  100%{
+    left:110%;
+    transform:rotate(45deg);
+  }
+}
+@keyframes starTwinkle{
+  0%{
+     background:rgba(255,255,255,0.4);
+  }
+  25%{
+    background:rgba(255,255,255,0.8);
+  }
+  50%{
+   background:rgba(255,255,255,1);
+  }
+  75%{
+    background:rgba(255,255,255,0.8);
+  }
+  100%{
+    background:rgba(255,255,255,0.4);
+  }
+}
+    </style>
+</head>
+<body>
+    <div class="text">
+        <div>ERROR</div>
+        <h1>404</h1>
+        <hr>
+        <div class="mb-2">Page Not Found</div>
+        <a href="{{ url()->previous() }}" class="btn btn-outline-primary mb-3 ">Voltar</a>
+    </div>
+
+      <div class="astronaut">
+        <img src="https://images.vexels.com/media/users/3/152639/isolated/preview/506b575739e90613428cdb399175e2c8-space-astronaut-cartoon-by-vexels.png" alt="" class="src">
+      </div>
+</body>
+<script>
+    document.addEventListener("DOMContentLoaded",function(){
+
+  var body=document.body;
+   setInterval(createStar,100);
+   function createStar(){
+     var right=Math.random()*500;
+     var top=Math.random()*screen.height;
+     var star=document.createElement("div");
+  star.classList.add("star")
+   body.appendChild(star);
+   setInterval(runStar,10);
+     star.style.top=top+"px";
+   function runStar(){
+     if(right>=screen.width){
+       star.remove();
+     }
+     right+=3;
+     star.style.right=right+"px";
+   }
+   }
+ })
+</script>
+</html>
