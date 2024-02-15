9fe822cfa8625e10110bf3fff38b03f60501ab8e
Deadfaced <btteixeira.bt@gmail.com>
2024-02-15 19:53:46 +0000

    fixed login card size to change according with screen size

--- Laravel/resources/views/login/login.blade.php
+++ Laravel/resources/views/login/login.blade.php
@@ -2,8 +2,8 @@
 
 <div class="container mx-auto d-flex align-items-center loginPage w-100 fade-in">
     <div class="d-flex justify-content-center w-100 ">
-        <div class="card" style="border-radius: 2rem;">
-                <div class="d-flex flex-column justify-content-between align-items-center h-100" style="min-height: 37rem; min-width: 35rem; padding: 5rem;">
+        <div class="card" style="border-radius: 2rem; min-width: 50%;">
+                <div class="d-flex flex-column justify-content-between align-items-center h-100 w-100" style="min-height: 70%; padding: 5rem;">
                     <div class="d-flex flex-column align-items-center h-25">
                         <img src="{{ asset('assets/logo.png') }}" alt="Logo">
                         <h5 class="mt-3"><strong> ATEC GEST PRO </strong></h5>
