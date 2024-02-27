1a5123e58d242d72cefd684ee25ff6f971044a57
Wilson Miranda <wilson.miranda.t0123188@edu.atec.pt>
2024-02-27 18:11:06 +0000

    Add countdown redirect function to error.js

--- Laravel/public/js/error.js
+++ Laravel/public/js/error.js
@@ -1,1 +1,15 @@
+function countdownRedirect() {
+    let seconds = 5;
+    let countdownElement = document.getElementById("countdown");
+    let countdownInterval = setInterval(function() {
+        seconds--;
+        countdownElement.textContent = seconds;
+        if (seconds <= 0) {
+            clearInterval(countdownInterval);
+            window.location.href = "/";
+        }
+    }, 1000);
+}
+
+countdownRedirect();
 
