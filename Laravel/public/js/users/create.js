77c026f37594484ff082ea2315c999eb0f0df5dc
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2024-02-27 15:49:18 +0000

    Logic of the user creation form updated to make it possible to view the password.

--- Laravel/public/js/users/create.js
+++ Laravel/public/js/users/create.js
@@ -1,3 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const passwordInput = document.getElementById('password');
+    const togglePasswordBtn = document.getElementById('togglePassword');
+    const eyeIcon = togglePasswordBtn.querySelector('i');
+
+    togglePasswordBtn.addEventListener('click', function () {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+
+        eyeIcon.classList.toggle('fa-eye', type === 'password');
+        eyeIcon.classList.toggle('fa-eye-slash', type === 'text');
+    });
+});
+
 function toggleCourseClassDiv() {
     const selectedRole = $("#role_id").val();
     const isStudentValue = selectedRole === "3" ? 1 : 0;
@@ -13,12 +27,12 @@ function toggleCourseClassDiv() {
     }
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     toggleCourseClassDiv();
 });
 
-window.setTimeout(function() {
-    $(".alert-danger").fadeTo(500, 0).slideUp(500, function() {
+window.setTimeout(function () {
+    $(".alert-danger").fadeTo(500, 0).slideUp(500, function () {
         $(this).remove();
     });
 }, 3000);
