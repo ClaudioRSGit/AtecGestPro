32d1658f0e183659bfe0cfb7f33f517d3e7d64a3
Wilson Miranda <wilson.miranda.t0123188@edu.atec.pt>
2024-02-27 18:09:56 +0000

    split js and css

--- Laravel/resources/views/errors/419.blade.php
+++ Laravel/resources/views/errors/419.blade.php
@@ -7,6 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
+         <link href="{{ asset('css/errorstyles.css') }}" rel="stylesheet">
+
 </head>
 <body>
 <div class="container">
@@ -23,6 +25,11 @@
 </div>
 
 
+<div class="error-image">
+    <img src="{{ asset('assets/404.png') }}">
+</div>
+
+<script src="{{ asset('js/error.js') }}"></script>
 
 </body>
 </html>
