9ba1a31fc01e883aa73b70f08a5c49f4691c19c7
FabioLazera <fabio.silva.t0111864@edu.atec.pt>
2024-02-28 00:07:34 +0000

    Update MaterialSeeder.php

--- Laravel/database/seeds/MaterialSeeder.php
+++ Laravel/database/seeds/MaterialSeeder.php
@@ -23,42 +23,11 @@ class MaterialSeeder extends Seeder
             'gender' => null,
 
         ]);
-        DB::table('materials')->insert([
-            'name' => 'Martelo',
-            'description' => 'Para martelar',
-            'isInternal' => 0,
-            'quantity' => 5,
-            'acquisition_date' => '2020-12-09 21:04:24',
-            'supplier' => 'Ferragens do Zé',
-            'isClothing' => 0,
-            'gender' => null,
 
-        ]);
+        // Seed 2
         DB::table('materials')->insert([
-            'name' => 'Martelo',
-            'description' => 'Para martelar',
-            'isInternal' => 0,
-            'quantity' => 5,
-            'acquisition_date' => '2020-12-09 21:04:24',
-            'supplier' => 'Ferragens do Zé',
-            'isClothing' => 0,
-            'gender' => null,
-
-        ]);
-        DB::table('materials')->insert([
-            'name' => 'Martelo',
-            'description' => 'Para martelar',
-            'isInternal' => 0,
-            'quantity' => 5,
-            'acquisition_date' => '2020-12-09 21:04:24',
-            'supplier' => 'Ferragens do Zé',
-            'isClothing' => 0,
-            'gender' => null,
-
-        ]);
-        DB::table('materials')->insert([
-            'name' => 'Martelo',
-            'description' => 'Para martelar',
+            'name' => 'Chave de Fendas',
+            'description' => 'Para apertar/desapertar parafusos',
             'isInternal' => 0,
             'quantity' => 5,
             'acquisition_date' => '2020-12-09 21:04:24',
@@ -70,6 +39,45 @@ class MaterialSeeder extends Seeder
 
         // Seed 3
         DB::table('materials')->insert([
+            'name' => 'Chave Inglesa',
+            'description' => 'Para apertar/desapertar fêmeas',
+            'isInternal' => 0,
+            'quantity' => 5,
+            'acquisition_date' => '2020-12-09 21:04:24',
+            'supplier' => 'Ferragens do Zé',
+            'isClothing' => 0,
+            'gender' => null,
+
+        ]);
+
+        // Seed 4
+        DB::table('materials')->insert([
+            'name' => 'Serrote',
+            'description' => 'Para cortar',
+            'isInternal' => 0,
+            'quantity' => 5,
+            'acquisition_date' => '2020-12-09 21:04:24',
+            'supplier' => 'Ferragens do Zé',
+            'isClothing' => 0,
+            'gender' => null,
+
+        ]);
+
+        // Seed 5
+        DB::table('materials')->insert([
+            'name' => 'Alicate',
+            'description' => 'Multiusos',
+            'isInternal' => 0,
+            'quantity' => 5,
+            'acquisition_date' => '2020-12-09 21:04:24',
+            'supplier' => 'Ferragens do Zé',
+            'isClothing' => 0,
+            'gender' => null,
+
+        ]);
+
+        // Seed 6
+        DB::table('materials')->insert([
             'name' => 'Lampada',
             'description' => 'T18 36W',
             'isInternal' => 1,
@@ -80,35 +88,50 @@ class MaterialSeeder extends Seeder
             'gender' => null,
 
         ]);
-        // Seed 4
+
+        // Seed 7
         DB::table('materials')->insert([
             'name' => 'Compressor',
             'description' => '50L',
             'isInternal' => 0,
-            'quantity' => 8,
+            'quantity' => 3,
             'acquisition_date' => null,
             'supplier' => null,
             'isClothing' => 0,
             'gender' => null,
 
         ]);
-        // Seed 5
+
+        // Seed 8
         DB::table('materials')->insert([
             'name' => 'Televisão',
             'description' => '32 Polegadas',
             'isInternal' => 0,
             'quantity' => 8,
             'acquisition_date' => null,
-            'supplier' => null,
+            'supplier' => 'Worten',
             'isClothing' => 0,
             'gender' => null,
 
         ]);
 
-        // Seed 6
+        // Seed 9
         DB::table('materials')->insert([
             'name' => 'Tshirt',
-            'description' => 'Tshirt homem',
+            'description' => 'T-shirt Homem',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'acquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => null,
+            'isClothing' => 1,
+            'gender' => 1,
+
+        ]);
+
+        // Seed 10
+        DB::table('materials')->insert([
+            'name' => 'Tshirt',
+            'description' => 'T-shirt Senhora',
             'isInternal' => 1,
             'quantity' => 10,
             'acquisition_date' => '2020-01-09 21:04:24',
@@ -118,11 +141,23 @@ class MaterialSeeder extends Seeder
 
         ]);
 
-
-        // Seed 7
+        // Seed 11
         DB::table('materials')->insert([
-            'name' => 'Tshirt',
-            'description' => 'Tshirt mulher',
+            'name' => 'Sweat azul',
+            'description' => 'Sweat azul Homem',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'acquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => null,
+            'isClothing' => 1,
+            'gender' => 1,
+
+        ]);
+
+        // Seed 12
+        DB::table('materials')->insert([
+            'name' => 'Polar',
+            'description' => 'Polar Homem',
             'isInternal' => 1,
             'quantity' => 10,
             'acquisition_date' => '2020-01-09 21:04:24',
@@ -133,110 +168,40 @@ class MaterialSeeder extends Seeder
         ]);
 
 
-        // Seed 8
-        DB::table('materials')->insert([
-            'name' => 'Sweat azul',
-            'description' => 'Sweat azul homem',
-            'isInternal' => 1,
-            'quantity' => 10,
-            'acquisition_date' => '2020-01-09 21:04:24',
-            'supplier' => '',
-            'isClothing' => 1,
-            'gender' => 0,
-
-        ]);
-
-
-        // Seed 9
-        DB::table('materials')->insert([
-            'name' => 'Polar',
-            'description' => 'Polar homem',
-            'isInternal' => 1,
-            'quantity' => 10,
-            'acquisition_date' => '2020-01-09 21:04:24',
-            'supplier' => '',
-            'isClothing' => 1,
-            'gender' => 0,
-
-        ]);
-
-
-        // Seed 10
+        // Seed 13
         DB::table('materials')->insert([
             'name' => 'Camisa soldador',
-            'description' => 'Camisa soldador homem',
+            'description' => 'Camisa soldador Homem',
             'isInternal' => 1,
             'quantity' => 10,
             'acquisition_date' => '2020-01-09 21:04:24',
-            'supplier' => '',
+            'supplier' => null,
             'isClothing' => 1,
-            'gender' => 0,
+            'gender' => 1,
 
         ]);
 
-
-        // Seed 11
+        // Seed 14
         DB::table('materials')->insert([
             'name' => 'Calças castanhas mécatrónica',
-            'description' => 'Calças castanhas mécatrónica homem',
+            'description' => 'Calças castanhas mécatrónica Homem',
             'isInternal' => 1,
             'quantity' => 10,
             'acquisition_date' => '2020-01-09 21:04:24',
-            'supplier' => '',
+            'supplier' => null,
             'isClothing' => 1,
-            'gender' => 0,
+            'gender' => 1,
 
         ]);
 
-
-        // Seed 12
+        // Seed 15
         DB::table('materials')->insert([
             'name' => 'Sapato de segurança',
             'description' => 'Sapato de segurança',
             'isInternal' => 1,
             'quantity' => 10,
             'acquisition_date' => '2020-01-09 21:04:24',
-            'supplier' => '',
-            'isClothing' => 1,
-            'gender' => null,
-
-        ]);
-
-        // Seed 13
-        DB::table('materials')->insert([
-            'name' => 'Avental soldadura',
-            'description' => 'Avental soldadura',
-            'isInternal' => 1,
-            'quantity' => 10,
-            'acquisition_date' => '2020-01-09 21:04:24',
-            'supplier' => '',
-            'isClothing' => 1,
-            'gender' => null,
-
-        ]);
-
-        // Seed 14
-        DB::table('materials')->insert([
-            'name' => 'Capuz soldadura',
-            'description' => 'Capuz soldadura',
-            'isInternal' => 1,
-            'quantity' => 10,
-            'acquisition_date' => '2020-01-09 21:04:24',
-            'supplier' => '',
-            'isClothing' => 1,
-            'gender' => null,
-
-        ]);
-
-
-        // Seed 15
-        DB::table('materials')->insert([
-            'name' => 'Luvas soldadura',
-            'description' => 'Luvas soldadura',
-            'isInternal' => 1,
-            'quantity' => 10,
-            'acquisition_date' => '2020-01-09 21:04:24',
-            'supplier' => '',
+            'supplier' => null,
             'isClothing' => 1,
             'gender' => null,
 
@@ -244,26 +209,25 @@ class MaterialSeeder extends Seeder
 
         // Seed 16
         DB::table('materials')->insert([
-            'name' => 'Calças cinzas de soldador',
-            'description' => 'Calças cinzas de soldador homem',
+            'name' => 'Avental soldadura',
+            'description' => 'Avental soldadura',
             'isInternal' => 1,
             'quantity' => 10,
             'acquisition_date' => '2020-01-09 21:04:24',
-            'supplier' => '',
+            'supplier' => null,
             'isClothing' => 1,
-            'gender' => 0,
+            'gender' => null,
 
         ]);
 
-
         // Seed 17
         DB::table('materials')->insert([
-            'name' => 'Botas de soldador',
-            'description' => 'Botas de soldador',
+            'name' => 'Capuz soldadura',
+            'description' => 'Capuz soldadura',
             'isInternal' => 1,
             'quantity' => 10,
             'acquisition_date' => '2020-01-09 21:04:24',
-            'supplier' => '',
+            'supplier' => null,
             'isClothing' => 1,
             'gender' => null,
 
@@ -271,34 +235,60 @@ class MaterialSeeder extends Seeder
 
         // Seed 18
         DB::table('materials')->insert([
-            'name' => 'Casaco de croute',
-            'description' => 'Casaco de croute homem',
+            'name' => 'Luvas soldadura',
+            'description' => 'Luvas soldadura',
             'isInternal' => 1,
             'quantity' => 10,
             'acquisition_date' => '2020-01-09 21:04:24',
-            'supplier' => '',
+            'supplier' => null,
             'isClothing' => 1,
-            'gender' => 0,
+            'gender' => null,
 
         ]);
 
         // Seed 19
         DB::table('materials')->insert([
-            'name' => 'Bata beje',
-            'description' => 'Bata beje homem',
+            'name' => 'Calças cinzas de soldador',
+            'description' => 'Calças cinzas de soldador Homem',
             'isInternal' => 1,
             'quantity' => 10,
             'acquisition_date' => '2020-01-09 21:04:24',
-            'supplier' => 'Fardas e Companhia',
+            'supplier' => null,
             'isClothing' => 1,
-            'gender' => 0,
+            'gender' => 1,
 
         ]);
 
         // Seed 20
         DB::table('materials')->insert([
+            'name' => 'Botas de soldador',
+            'description' => 'Botas de soldador',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'acquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => null,
+            'isClothing' => 1,
+            'gender' => null,
+
+        ]);
+
+        // Seed 21
+        DB::table('materials')->insert([
+            'name' => 'Casaco de croute',
+            'description' => 'Casaco de croute Homem',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'acquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => '',
+            'isClothing' => 1,
+            'gender' => 1,
+
+        ]);
+
+        // Seed 22
+        DB::table('materials')->insert([
             'name' => 'Bata beje',
-            'description' => 'Bata beje mulher',
+            'description' => 'Bata beje Homem',
             'isInternal' => 1,
             'quantity' => 10,
             'acquisition_date' => '2020-01-09 21:04:24',
@@ -308,5 +298,16 @@ class MaterialSeeder extends Seeder
 
         ]);
 
+        // Seed 23
+        DB::table('materials')->insert([
+            'name' => 'Bata beje',
+            'description' => 'Bata beje Senhora',
+            'isInternal' => 1,
+            'quantity' => 10,
+            'acquisition_date' => '2020-01-09 21:04:24',
+            'supplier' => 'Fardas e Companhia',
+            'isClothing' => 1,
+            'gender' => 0,
+        ]);
     }
 }
