3a92393a51c202dd7588e85932914240efc65a02
Claudio <claudio.silva.t0123173@edu.atec.pt>
2024-02-29 15:35:44 +0000

    merging to main

--- Laravel/database/seeds/TicketHistorySeeder.php
+++ Laravel/database/seeds/TicketHistorySeeder.php
@@ -14,35 +14,35 @@ class TicketHistorySeeder extends Seeder
         // Seed 1
         DB::table('ticket_histories')->insert([
             'ticket_id' => 1,
-            'ticket_info' => 'Ticket created.',
+            'ticket_info' => 'Ticket criado.',
             'action_id' => 1,
         ]);
 
         // Seed 2
         DB::table('ticket_histories')->insert([
             'ticket_id' => 2,
-            'ticket_info' => 'Ticket created.',
+            'ticket_info' => 'Ticket criado.',
             'action_id' => 1,
         ]);
 
         // Seed 3
         DB::table('ticket_histories')->insert([
             'ticket_id' => 3,
-            'ticket_info' => 'Ticket created.',
+            'ticket_info' => 'Ticket criado.',
             'action_id' => 1,
         ]);
 
         // Seed 4
         DB::table('ticket_histories')->insert([
             'ticket_id' => 4,
-            'ticket_info' => 'Ticket created.',
+            'ticket_info' => 'Ticket criado.',
             'action_id' => 1,
         ]);
 
         // Seed 5
         DB::table('ticket_histories')->insert([
             'ticket_id' => 5,
-            'ticket_info' => 'Ticket created.',
+            'ticket_info' => 'Ticket criado.',
             'action_id' => 3,
         ]);
     }
